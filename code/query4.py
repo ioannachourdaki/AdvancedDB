18fc7197fbe5eee0b5af7d9e5364ae88e9a34a1b
ioannachourdaki <102799661+ioannachourdaki@users.noreply.github.com>
2024-01-15 14:15:40 +0200

    Update query4.py

--- code/query4.py
+++ code/query4.py
@@ -4,7 +4,6 @@ from pyspark.sql.functions import col, to_date, when, substring
 from pyspark.sql import functions as F
 from pyspark.sql.functions import udf
 import math
-import time
 
 # Function for diastance calculation
 def get_distance(lat1, lon1, lat2, lon2):
@@ -70,51 +69,35 @@ spark.sql(view2)
 ### Query 4a (1) ###
 
 query4a1 = """
-SELECT YEAR(c.`DATE OCC`) AS Year, AVG(get_distance(c.LAT, c.LON, p.Y, p.X)) AS `Average Distance`, COUNT(*) AS Count
+SELECT YEAR(c.`DATE OCC`) AS Year,  ROUND(AVG(get_distance(c.LAT, c.LON, p.Y, p.X)), 3) AS `Average Distance`, COUNT(*) AS Count
 FROM CrimeView1 AS c
 INNER JOIN Precincts p ON c.AREA = p.PREC
 GROUP BY Year
 ORDER BY Year
 """
 
-# Start timer
-start_time = time.time()
-
 query4a1_df = spark.sql(query4a1)
 query4a1_df.show()
-
-# Stop timer
-end_time = time.time()
-# Calculate the runtime
-print(f"SQL API runtime: {end_time - start_time} seconds")
 
 
 ### Query 4b (1) ###
 
 query4b1 = """
-SELECT p.DIVISION AS Division, AVG(get_distance(c.LAT, c.LON, p.Y, p.X)) AS `Average Distance`, COUNT(*) AS Count
+SELECT p.DIVISION AS Division,  ROUND(AVG(get_distance(c.LAT, c.LON, p.Y, p.X)), 3) AS `Average Distance`, COUNT(*) AS Count
 FROM CrimeView2 AS c
 INNER JOIN Precincts p ON c.AREA = p.PREC
 GROUP BY Division
 ORDER BY Count DESC;
 """
 
-# Start timer
-start_time = time.time()
-
 query4b1_df = spark.sql(query4b1)
 query4b1_df.show()
-
-# Stop timer
-end_time = time.time()
-# Calculate the runtime
-print(f"SQL API runtime: {end_time - start_time} seconds")
 
 
 ### Query 4a (2) ###
 
 query4a2 = """
-SELECT Year, AVG(`Min Distance`) AS `Average Distance`, COUNT(*) AS Count
+SELECT Year,  ROUND(AVG(`Min Distance`), 3) AS `Average Distance`, COUNT(*) AS Count
 FROM (
         SELECT c.DR_NO, YEAR(c.`DATE OCC`) AS Year, MIN(get_distance(c.LAT, c.LON, p.Y, p.X)) AS `Min Distance`
         FROM CrimeView1 AS c
@@ -125,44 +108,28 @@ GROUP BY Year
 ORDER BY Year
 """
 
-# Start timer
-start_time = time.time()
-
 query4a2_df = spark.sql(query4a2)
 query4a2_df.show()
-
-# Stop timer
-end_time = time.time()
-# Calculate the runtime
-print(f"SQL API runtime: {end_time - start_time} seconds")
 
 
 ### Query 4b (2) ###
 
 query4b2 = """
-SELECT t1.DIVISION AS Division, AVG(t2.Distance) AS `Average Distance`, COUNT(*) AS Count
+SELECT t1.DIVISION AS Division,  ROUND(AVG(t2.Min_Distance), 3) AS `Average Distance`, COUNT(*) AS Count
 FROM (
         SELECT c.DR_NO, p.DIVISION, get_distance(c.LAT, c.LON, p.Y, p.X) AS Distance
         FROM CrimeView2 AS c
         CROSS JOIN Precincts p
     ) AS t1
 INNER JOIN (
-        SELECT c.DR_NO, MIN(get_distance(c.LAT, c.LON, p.Y, p.X)) AS Distance
+        SELECT c.DR_NO, MIN(get_distance(c.LAT, c.LON, p.Y, p.X)) AS Min_Distance
         FROM CrimeView2 AS c
         CROSS JOIN Precincts p
         GROUP BY DR_NO
-    ) AS t2 ON t1.DR_NO = t2.DR_NO AND t1.Distance = t2.Distance
+    ) AS t2 ON t1.DR_NO = t2.DR_NO AND t1.Distance = t2.Min_Distance
 GROUP BY Division
 ORDER BY Count DESC;
 """
 
-# Start timer
-start_time = time.time()
-
 query4b2_df = spark.sql(query4b2)
 query4b2_df.show()
-
-# Stop timer
-end_time = time.time()
-# Calculate the runtime
-print(f"SQL API runtime: {end_time - start_time} seconds")
